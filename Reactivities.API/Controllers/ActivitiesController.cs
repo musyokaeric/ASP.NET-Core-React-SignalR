f39f1e92fcb37a1c07f52cfae8b9a6a924164b6e
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-02 18:28:44 +0300

    Added API Controller

---
+++ Reactivities.API/Controllers/ActivitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities() => await context.Activities.ToListAsync();
+
+        [HttpGet("{id}")] //api/activities/{id}
+        public async Task<ActionResult<Activity>> GetActivity(Guid id) => await context.Activities.FindAsync(id);
+    }
+}
