e30f9c4af824d78b4bb19cd5724b312c61096ec7
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-04 19:55:34 +0300

    Added Activity Interface
    Taking advantage of type safety

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
 
-    const [activities, setActivities] = useState([]);
+    const [activities, setActivities] = useState<Activity[]>([]);
 
     useEffect(() => {
-        axios.get('https://localhost:7000/api/activities')
+        axios.get<Activity[]>('https://localhost:7000/api/activities')
             .then(response => setActivities(response.data))
             .catch(error => console.error(error))
     }, [])
 
     return (
         <>
-            <Header as='h2' icon='users' content='Reactivities'/>
+            <Header as='h2' icon='users' content='Reactivities' />
             <List>
                 {
-                    activities.map((activity: any, index) => <List.Item key={index}>{activity.title}</List.Item>)
+                    activities.map((activity: Activity, index) => <List.Item key={index}>{activity.title}</List.Item>)
                 }
             </List>
         </>
