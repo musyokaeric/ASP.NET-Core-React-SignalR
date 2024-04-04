3d3817161e67359b3590b5cbd5c4eeb611e8e07c
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-04 20:55:41 +0300

    Created the Activity Details View

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,31 @@
+import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Icon, Image } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityDetails({ activity }: Props) {
+    return (
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+
+            <CardContent>
+                <CardHeader>{activity.title}</CardHeader>
+
+                <CardMeta>
+                    <span>{activity.date}</span>
+                </CardMeta>
+
+                <CardDescription>{activity.description}</CardDescription>
+            </CardContent>
+
+            <CardContent extra>
+                <Button.Group widths='2'>
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
+                </Button.Group>
+            </CardContent>
+        </Card>
+    )
+}
