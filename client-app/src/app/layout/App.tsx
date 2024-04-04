355959d5a27a86344f320cab736e2f92752f5dba
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-04 20:19:26 +0300

    Created NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
 
@@ -15,12 +16,15 @@ function App() {
 
     return (
         <>
-            <Header as='h2' icon='users' content='Reactivities' />
-            <List>
-                {
-                    activities.map((activity: Activity, index) => <List.Item key={index}>{activity.title}</List.Item>)
-                }
-            </List>
+            <NavBar />
+
+            <Container style={{ marginTop: '7em' }}>
+                <List>
+                    {
+                        activities.map((activity: Activity, index) => <List.Item key={index}>{activity.title}</List.Item>)
+                    }
+                </List>
+            </Container>
         </>
     )
 }
