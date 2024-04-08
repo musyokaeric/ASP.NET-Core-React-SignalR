af1b77837c26c99b8705f0ddadf593b92f978f87
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-08 19:58:48 +0300

    UI - Grouping Activities by Date
    Using the reduce() function

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,33 @@
-import { Item, Segment } from "semantic-ui-react"
+import { Header, Item, Segment } from "semantic-ui-react"
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react/jsx-runtime";
 
 
 export default observer(function ActivityList() {
     const { activityStore } = useStore();
-    const { activitiesByDate } = activityStore;
+    const { groupActivities } = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {
-                    activitiesByDate.map((activity) => (
-                        <ActivityListItem key={activity.id} activity={activity} />
-                    ))
-                }
-            </Item.Group>
-        </Segment>
+        <>
+            {
+                groupActivities.map(([group, activities])=>(
+                    <Fragment key={group }>
+                        <Header sub color='teal'>{group}</Header>
+
+                        <Segment>
+                            <Item.Group divided>
+                                {
+                                    activities.map((activity) => (
+                                        <ActivityListItem key={activity.id} activity={activity} />
+                                    ))
+                                }
+                            </Item.Group>
+                        </Segment>
+                    </Fragment>
+                ))
+            }
+        </>
     )
 })
