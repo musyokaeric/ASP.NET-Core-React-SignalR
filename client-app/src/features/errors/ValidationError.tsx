d18aaedc4635da61043a56a762ae8844d21e085c
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-11 19:29:05 +0300

    Handling 400 Errors on the Client

---
+++ client-app/src/features/errors/ValidationError.tsx
@@ -0,0 +1,22 @@
+import { Message } from "semantic-ui-react"
+
+interface Props {
+    errors: string[]
+}
+
+export default function ValidationError({ errors }: Props) {
+    return (
+        <Message error>
+            {
+                errors &&
+                <Message.List>
+                    {
+                        errors.map((error: string, index) => (
+                            <Message.Item key={index}>{error}</Message.Item>
+                        ))
+                    }
+                </Message.List>
+            }
+        </Message>
+    )
+}
