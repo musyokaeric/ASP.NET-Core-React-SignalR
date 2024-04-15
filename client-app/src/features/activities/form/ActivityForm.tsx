daf9ea2f6743c13360604d5cf3be4c84e95796c4
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-15 19:20:08 +0300

    Creating a Reusable Form Elements
    npm install react-datepicker
    
    npm install @types/react-datepicker --save-dev

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,17 @@
-import { Button, FormField, Label, Segment } from "semantic-ui-react";
+import { Button, Segment } from "semantic-ui-react";
 import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from 'yup';
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
+import MySelectInput from "../../../app/common/form/MySelectInput";
+import { categoryOptions } from "../../../app/common/options/categoryOptions";
+import MyDateInput from "../../../app/common/form/MyDateInput";
 
 export default observer(function ActivityForm() {
 
@@ -26,7 +31,12 @@ export default observer(function ActivityForm() {
     })
 
     const validationSchema = Yup.object({
-        title: Yup.string().required('The activity title is required')
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        city: Yup.string().required(),
+        venue: Yup.string().required(),
     })
 
     useEffect(() => {
@@ -62,16 +72,18 @@ export default observer(function ActivityForm() {
                 {
                     ({ handleSubmit }) => (
                         <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                            <FormField>
-                                <Field placeholder='Title' name='title' />
-                                <ErrorMessage name='title' render={error => <Label basic color='red' content={error} />} />
-                            </FormField>
+                            <MyTextInput name='title' placeholder='Title' />
 
-                            <Field placeholder='Description' name='description' />
-                            <Field placeholder='Category' name='category' />
-                            <Field type='date' placeholder='Date' name='date' />
-                            <Field placeholder='City' name='city' />
-                            <Field placeholder='Venue' name='venue' />
+                            <MyTextArea rows={3} placeholder='Description' name='description' />
+                            <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
+                            <MyDateInput
+                                placeholderText='Date'
+                                name='date'
+                                showTimeSelect
+                                timeCaption='time'
+                                dateFormat='MMMM dd, yyyy h:mm aa' />
+                            <MyTextInput placeholder='City' name='city' />
+                            <MyTextInput placeholder='Venue' name='venue' />
 
                             <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                             <Button as={Link} to={`/activities`} floated='right' type='button' content='Cancel' />
