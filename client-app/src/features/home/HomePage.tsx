b37cb0b0643fd720c4cd457df059e1943f5dd172
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-16 15:02:41 +0300

    Updated Home Page & Nav Bar

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const { userStore } = useStore();
+
     return (
         <Segment inverted textAlign='center' className='masthead'>
             <Container text>
@@ -10,12 +14,22 @@ export default function HomePage() {
                     Reactivities
                 </Header>
 
-                <Header as='h2' inverted content='Welcome to Reactivities' />
+                {
+                    userStore.isLoggedIn ? (
+                        <>
+                            <Header as='h2' inverted content='Welcome to Reactivities' />
 
-                <Button as={Link} to='/login' size='huge' inverted>
-                    Login!
-                </Button>
+                            <Button as={Link} to='/activities' size='huge' inverted>
+                                Go to Activities!
+                            </Button>
+                        </>
+                    ) : (
+                        <Button as={Link} to='/login' size='huge' inverted>
+                            Login!
+                        </Button>
+                    )
+                }
             </Container>
         </Segment>
     )
-}
+})
