da5ce3bdd610393360e167f4f3725a56f2f5f926
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-16 14:11:03 +0300

    Added Account Routes

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from "../models/activity";
 import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
+import { User, UserFormValues } from "../models/user";
 
 const sleep = (delay: number) => new Promise((resolve) => {
     setTimeout(resolve, delay)
@@ -71,8 +72,15 @@ const Activities = {
     delete: (id: string) => requests.delete<void>(`/activities/${id}`),
 }
 
+const Account = {
+    current: () => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+    register: (user: UserFormValues) => requests.post<User>('/account/register', user),
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent;
