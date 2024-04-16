333be8fc5d77c498d003113153418a18d8a0afd6
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-16 09:03:27 +0300

    Created the Account Controller

---
+++ Reactivities.API/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.API.DTOs;
+using Reactivities.Domain;
+
+namespace Reactivities.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+        {
+            var user = await userManager.FindByEmailAsync(loginDTO.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await userManager.CheckPasswordAsync(user, loginDTO.Password);
+
+            if (result)
+            {
+                return new UserDTO
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = "this will be a token",
+                    Userame = user.UserName,
+                };
+            }
+
+            return Unauthorized();
+        }
+    }
+}
