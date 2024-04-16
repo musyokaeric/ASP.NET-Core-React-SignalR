74ee6f72153d0271ba40e2a9bc0260b65d081c6a
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-16 22:17:46 +0300

    Added a Popover for Attendees

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -1,7 +1,8 @@
 import { observer } from "mobx-react-lite";
-import { Image, List } from "semantic-ui-react";
+import { Image, List, Popup } from "semantic-ui-react";
 import { Profile } from "../../../app/models/profile";
 import { Link } from "react-router-dom";
+import ProfileCard from "../../profiles/ProfileCard";
 
 interface Props {
     attendees: Profile[];
@@ -12,9 +13,17 @@ export default observer(function ActivityListItemAttendee({ attendees }: Props) 
         <List horizontal>
             {
                 attendees.map((attendee, index) => (
-                    <List.Item key={index} as={Link} to={`/profiles/${attendee.userName}`}>
-                        <Image size='mini' circular src={attendee.image || '/assets/user.png'} />
-                    </List.Item>
+                    <Popup hoverable key={index} trigger={
+                        <List.Item key={index} as={Link} to={`/profiles/${attendee.userName}`}>
+                            <Image size='mini' circular src={attendee.image || '/assets/user.png'} />
+                        </List.Item>
+                    }>
+                        <Popup.Content>
+                            <ProfileCard profile={attendee} />
+                        </Popup.Content>
+                    </Popup>
+
+
                 ))
             }
         </List>
