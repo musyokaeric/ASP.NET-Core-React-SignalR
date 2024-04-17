a071d6a4fab5dab0afa7db82d98dddaf61a69636
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-17 09:16:49 +0300

    Added Photo Entity
    Add-Migration PhotoEntityAdded -Project Reactivities.Persistence -StartupProject Reactivities.API

--- Reactivities.Persistence/DataContext.cs
+++ Reactivities.Persistence/DataContext.cs
@@ -13,6 +13,8 @@ namespace Reactivities.Persistence
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
 
+        public DbSet<Photo> Photos { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
