a40c488b6a2f156fae56325b561ec9e1e07442f9
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-17 16:16:49 +0300

    Getting the Profile Data

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -5,6 +5,7 @@ export interface IProfile {
     displayName: string;
     image?: string;
     bio?: string;
+    photos?: Photo[];
 }
 
 export class Profile implements IProfile {
@@ -12,10 +13,17 @@ export class Profile implements IProfile {
     displayName: string;
     image?: string;
     bio?: string;
+    photos?: Photo[];
 
     constructor(user: User) {
         this.userName = user.username;
         this.displayName = user.displayName;
         this.image = user.image;
     }
+}
+
+export interface Photo {
+    id: string;
+    url: string;
+    isMain: boolean;
 }
