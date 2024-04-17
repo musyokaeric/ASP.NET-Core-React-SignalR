800738dfe9a41d98986f3af45fc254ccddba8c2b
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2024-04-17 16:39:58 +0300

    Conditionally Rendering the Photo Widget

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,25 +1,52 @@
 import { observer } from "mobx-react-lite";
-import { Card, Header, Image, TabPane } from "semantic-ui-react";
+import { Button, Card, Grid, Header, Image, TabPane } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
+import { useState } from "react";
 
 interface Props {
     profile: Profile;
 }
 
 export default observer(function ProfilePhotos({ profile }: Props) {
+    const { profileStore: { isCurrentUser } } = useStore();
+
+    const [addPhotoMode, setAddPhotoMode] = useState(false);
+
     return (
         <TabPane>
-            <Header icon='image' content='Photos' />
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated='left' icon='image' content='Photos' />
 
-            <Card.Group itemsPerRow={5}>
-                {
-                    profile.photos?.map(photo => (
-                        <Card key={photo.id}>
-                            <Image src={photo.url} />
-                        </Card>
-                    ))
-                }
-            </Card.Group>
+                    {
+                        isCurrentUser && (
+                            <Button
+                                floated='right' basic
+                                content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+                                onClick={() => setAddPhotoMode(!addPhotoMode)} />
+                        )
+                    }
+                </Grid.Column>
+
+                <Grid.Column width={16}>
+                    {
+                        addPhotoMode ? (
+                            <p>Photo widget goes here</p>
+                        ) : (
+                            <Card.Group itemsPerRow={5}>
+                                {
+                                    profile.photos?.map(photo => (
+                                        <Card key={photo.id}>
+                                            <Image src={photo.url} />
+                                        </Card>
+                                    ))
+                                }
+                            </Card.Group>
+                        )
+                    }
+                </Grid.Column>
+            </Grid>
         </TabPane>
     )
 })
