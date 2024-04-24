ed810d2ddebdc08b9fffd05c1e98e63343381c4b
Eric Musyoka <musyokaeric@live.com>
2024-04-24 08:46:49 +0300

    Client Side Filtering

--- client-app/src/features/activities/dashboard/ActivityFilters.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -1,7 +1,11 @@
+import { observer } from "mobx-react-lite";
 import Calendar from "react-calendar";
 import { Header, Menu } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
 
-export default function ActivityFilters() {
+export default observer(function ActivityFilters() {
+    const { activityStore: { predicate, setPredicate } } = useStore();
+
     return (
         <>
             <Menu vertical size='large' style={{ width: '100%', marginTop: 25 }}>
@@ -9,14 +13,29 @@ export default function ActivityFilters() {
                     <Header icon='filter' color='teal' content='Filters' />
                 </Menu.Item>
 
-                <Menu.Item content='All Events' />
-                <Menu.Item content="I'm going" />
-                <Menu.Item content="I'm hosting" />
+                <Menu.Item
+                    content='All Events'
+                    active={predicate.has('all')}
+                    onClick={() => setPredicate('all', 'true')}
+                />
+                <Menu.Item
+                    active={predicate.has('isGoing')}
+                    onClick={() => setPredicate('isGoing', 'true')}
+                    content="I'm going"
+                />
+                <Menu.Item
+                    content="I'm hosting"
+                    active={predicate.has('isHost')}
+                    onClick={() => setPredicate('isHost', 'true')}
+                />
             </Menu>
 
             <Header icon='calendar' attached color='teal' content='Select date' />
 
-            <Calendar />
+            <Calendar
+                onChange={(date) => setPredicate('startDate', date as Date)}
+                value={predicate.get('startDate') || new Date()}
+            />
         </>
     )
-}
+})
