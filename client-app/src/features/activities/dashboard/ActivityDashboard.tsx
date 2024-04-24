a5f972484e6db39b55fa3a3699d335cf1da41eb7
Eric Musyoka <musyokaeric@live.com>
2024-04-24 08:55:22 +0300

    Added Loading Placeholders

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,6 +7,7 @@ import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilters from "./ActivityFilters";
 import { PagingParams } from "../../../app/models/pagination";
 import InfiniteScroll from "react-infinite-scroller";
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder";
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
@@ -24,19 +25,27 @@ export default observer(function ActivityDashboard() {
         if (activityRegistry.size <= 0) loadActivities()
     }, [loadActivities, activityRegistry.size])
 
-    if (activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...' />
-
     return (
         <Grid>
             <Grid.Column width='10'>
-                <InfiniteScroll
-                    pageStart={0}
-                    loadMore={handleGetNext}
-                    hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-                    initialLoad={false}
-                >
-                    <ActivityList />
-                </InfiniteScroll>
+                {
+                    activityStore.loadingInitial && activityRegistry.size === 0 && !loadingNext ? (
+                        <>
+                            <ActivityListItemPlaceholder />
+                            <ActivityListItemPlaceholder />
+                        </>
+                    ) : (
+                        <InfiniteScroll
+                            pageStart={0}
+                            loadMore={handleGetNext}
+                            hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+                            initialLoad={false}
+                        >
+                            <ActivityList />
+                        </InfiniteScroll>
+                    )
+                }
+
             </Grid.Column>
 
             <Grid.Column width='6'>
