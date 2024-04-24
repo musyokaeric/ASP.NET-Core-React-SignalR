85a04592ca06bf4842e97b47a6b0a8edbfa29203
Eric Musyoka <musyokaeric@live.com>
2024-04-24 10:34:17 +0300

    Added Scroll to Top & Private Routes

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -6,8 +6,8 @@ import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
-import LoginForm from "../../features/users/LoginForm";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
     {
@@ -15,16 +15,18 @@ export const routes: RouteObject[] = [
         path: '/',
         element: <App />,
         children: [
-            { path: 'activities', element: <ActivityDashboard /> },
-            { path: 'activities/:id', element: <ActivityDetails /> },
-            { path: 'createActivity', element: <ActivityForm key='create' /> },
-            { path: 'manage/:id', element: <ActivityForm key='manage' /> },
+            {
+                element: <RequireAuth />, children: [
+                    { path: 'activities', element: <ActivityDashboard /> },
+                    { path: 'activities/:id', element: <ActivityDetails /> },
+                    { path: 'createActivity', element: <ActivityForm key='create' /> },
+                    { path: 'manage/:id', element: <ActivityForm key='manage' /> },
 
-            { path: 'profiles/:username', element: <ProfilePage /> },
+                    { path: 'profiles/:username', element: <ProfilePage /> },
 
-            { path: 'login', element: <LoginForm /> },
+                    { path: 'errors', element: <TestErrors /> },
+            ]},
 
-            { path: 'errors', element: <TestErrors /> },
             { path: 'not-found', element: <NotFound /> },
             { path: '*', element: <Navigate replace to='/not-found' /> }, // whenever a user types a bad url
             { path: 'server-error', element: <ServerError /> },
